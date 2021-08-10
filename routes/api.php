fcd5f356af1f13c139828a029423ed6e0a78c934
root <root@jgnr68-105.host.com>
2021-08-10 15:57:33 +0800

    Feat:api,DomainController

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DomainController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,5 +20,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::prefix('domains')->group(function () {
-  Route::get('list', [DomainController::class, 'index']);
+  Route::get('/', [DomainController::class, 'index']);
 });
