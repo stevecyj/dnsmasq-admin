777ec9e614bd22a4c52f42e11fecb4e568ee470c
root <root@jgnr68-105.host.com>
2021-08-10 10:17:39 +0800

    Feat:configures_table

---
+++ database/migrations/2021_08_10_021534_create_configures_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConfiguresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('configures', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unique()->comment('配置名');
+            $table->json('content')->comment('配置内容');
+            $table->string('remark')->nullable()->comment('備註');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('configures');
+    }
+}
